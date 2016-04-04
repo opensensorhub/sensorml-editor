144657135430d7064201a71ceccc453d2b6d8c70
Mathieu dhainaut <mathieu.dhainaut@gmail.com>
2016-04-04 17:20:34 +0200

    #issue85: add support of advanced panel for TypeOf

--- src/com/sensia/tools/client/swetools/editors/sensorml/panels/widgets/sml/SMLSensorEventWidget.java
+++ src/com/sensia/tools/client/swetools/editors/sensorml/panels/widgets/sml/SMLSensorEventWidget.java
@@ -1,13 +1,17 @@
 package com.sensia.tools.client.swetools.editors.sensorml.panels.widgets.sml;
 
+import java.util.Arrays;
 import java.util.List;
 
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 import com.sensia.tools.client.swetools.editors.sensorml.SensorConstants;
+import com.sensia.tools.client.swetools.editors.sensorml.listeners.IButtonCallback;
+import com.sensia.tools.client.swetools.editors.sensorml.listeners.ICallback;
 import com.sensia.tools.client.swetools.editors.sensorml.panels.widgets.AbstractSensorElementWidget;
 import com.sensia.tools.client.swetools.editors.sensorml.panels.widgets.ISensorWidget;
 import com.sensia.tools.client.swetools.editors.sensorml.panels.widgets.ISensorWidget.APPENDER;
@@ -26,6 +30,10 @@ public class SMLSensorEventWidget extends AbstractSensorElementWidget{
 	public SMLSensorEventWidget() {
 		super("Event", TAG_DEF.SML, TAG_TYPE.ELEMENT);
 		container = new HorizontalPanel();
+		init();
+	}
+
+	private void init() {
 		defPanel = new HorizontalPanel();
 		labelPanel = new HorizontalPanel();
 		timePanel = new HorizontalPanel();
@@ -34,8 +42,18 @@ public class SMLSensorEventWidget extends AbstractSensorElementWidget{
 		container.add(defPanel);
 		container.add(new HTML("-->"+SensorConstants.HTML_SPACE));
 		container.add(timePanel);
+		
+		Panel advancedPanel = getEditPanel(new IButtonCallback() {
+			@Override
+			public void onClick() {
+				refreshChildren(getElements());
+				refreshParents(getParent());
+			}
+		});
+		
+		container.add(advancedPanel);
 	}
-
+	
 	@Override
 	public Panel getPanel() {
 		return container;
--- src/com/sensia/tools/client/swetools/editors/sensorml/panels/widgets/sml/SensorSectionWidget.java
+++ src/com/sensia/tools/client/swetools/editors/sensorml/panels/widgets/sml/SensorSectionWidget.java
@@ -11,6 +11,7 @@ import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
+import com.sensia.tools.client.swetools.editors.sensorml.listeners.IButtonCallback;
 import com.sensia.tools.client.swetools.editors.sensorml.panels.widgets.AbstractSensorElementWidget;
 import com.sensia.tools.client.swetools.editors.sensorml.panels.widgets.ISensorWidget;
 import com.sensia.tools.client.swetools.editors.sensorml.panels.widgets.ISensorWidget.MODE;
@@ -64,78 +65,11 @@ public class SensorSectionWidget extends AbstractSensorElementWidget{
 	@Override
 	protected void addSensorWidget(ISensorWidget widget) {
 		if(getName().equals("characteristics") || getName().equals("capabilities")) {
-			
-			// 3 cases:
-			// 1) it's the name attribute
-			// 2) it's the CharacteristicList/CapabilityList with/without definition
-			// 3) the List contains a label
-			
-			String valueHeader = getName();
-			//case 1 : it's the name attribute
-			
-			//TODO:May be handled by a separator ISensorWidget?
-			if(widget.getType() == TAG_TYPE.ATTRIBUTE && widget.getName().equals("name")) {
-				valueHeader += " ("+toNiceLabel(widget.getValue(widget.getName(), true))+")";
-				currentHeader.setText(valueHeader);
-			} else if(widget.getName().equals("CharacteristicList") || widget.getName().equals("CapabilityList")) {
-				//looking for definition and label
-				List<ISensorWidget> children = widget.getElements();
-				for(ISensorWidget child : children) {
-					if(child.getType() == TAG_TYPE.ATTRIBUTE && child.getName().equals("definition")) {
-						
-						child.getPanel().removeFromParent();
-						
-						HorizontalPanel headerPanel = new HorizontalPanel();
-						Widget currentHeader = hidePanel.getHeader();
-						headerPanel.add(currentHeader);
-						headerPanel.add(child.getPanel());
-						
-						child.getPanel().addStyleName("icon-def-section-header");
-						hidePanel.setHeader(headerPanel);
-					} else if(child.getType() == TAG_TYPE.ELEMENT && child.getName().equals("label")) {
-						valueHeader += " ("+toNiceLabel(child.getValue(child.getName(), true))+")";
-						currentHeader.setText(valueHeader);
-						child.getPanel().removeFromParent();
-						break;
-					}
-				}
-				Panel panel = widget.getPanel();
-				panel.addStyleName("swe-generic-vertical-panel");
-				contentPanel.add(panel);
-			} else {
-				Panel panel = widget.getPanel();
-				panel.addStyleName("swe-generic-vertical-panel");
-				contentPanel.add(panel);
-			}
+			handleCharacteristic(widget);
 		} else if(getName().equals("typeOf")) {
-			if(widget.getType() == TAG_TYPE.ATTRIBUTE) {
-				if(widget.getName().equals("title")) {
-					linkName = widget.getValue("title", true);
-				} else if(widget.getName().equals("href")) {
-					href = widget.getValue("href", true);
-				}
-			}
-			if(getElements().isEmpty()) {
-				anchor = new Anchor("","");
-				VerticalPanel vPanel = new VerticalPanel();
-				vPanel.add(anchor);
-				vPanel.addStyleName("swe-generic-vertical-panel");
-				contentPanel.add(vPanel);
-			} else {
-				//generate client link url
-				anchor.setHref(Utils.getCurrentURL(href));
-				anchor.setText(linkName);
-			}
-			
-			anchor.setTarget("_blank");
+			handleTypeOf(widget);
 		} else if(widget.getType() == TAG_TYPE.ATTRIBUTE) {
-			//get header from child value
-			if(!widget.getElements().isEmpty()) {
-				//the first one should be a value widget
-				String value = widget.getElements().get(0).getName();
-				//set header
-				hidePanel.getHeaderTextAccessor().setText(value);
-			}
+			handleAttribute(widget);
 		} else {
 			Panel panel = widget.getPanel();
 			panel.addStyleName("swe-generic-vertical-panel");
@@ -143,6 +77,110 @@ public class SensorSectionWidget extends AbstractSensorElementWidget{
 		}
 	}
 
+	private void handleCharacteristic(ISensorWidget widget) {
+		// 3 cases:
+		// 1) it's the name attribute
+		// 2) it's the CharacteristicList/CapabilityList with/without definition
+		// 3) the List contains a label
+		
+		String valueHeader = getName();
+		//case 1 : it's the name attribute
+		
+		//TODO:May be handled by a separator ISensorWidget?
+		if(widget.getType() == TAG_TYPE.ATTRIBUTE && widget.getName().equals("name")) {
+			valueHeader += " ("+toNiceLabel(widget.getValue(widget.getName(), true))+")";
+			currentHeader.setText(valueHeader);
+		} else if(widget.getName().equals("CharacteristicList") || widget.getName().equals("CapabilityList")) {
+			//looking for definition and label
+			List<ISensorWidget> children = widget.getElements();
+			for(ISensorWidget child : children) {
+				if(child.getType() == TAG_TYPE.ATTRIBUTE && child.getName().equals("definition")) {
+					
+					child.getPanel().removeFromParent();
+					
+					HorizontalPanel headerPanel = new HorizontalPanel();
+					Widget currentHeader = hidePanel.getHeader();
+					headerPanel.add(currentHeader);
+					headerPanel.add(child.getPanel());
+					
+					child.getPanel().addStyleName("icon-def-section-header");
+					hidePanel.setHeader(headerPanel);
+				} else if(child.getType() == TAG_TYPE.ELEMENT && child.getName().equals("label")) {
+					valueHeader += " ("+toNiceLabel(child.getValue(child.getName(), true))+")";
+					currentHeader.setText(valueHeader);
+					child.getPanel().removeFromParent();
+					break;
+				}
+			}
+			Panel panel = widget.getPanel();
+			panel.addStyleName("swe-generic-vertical-panel");
+			contentPanel.add(panel);
+		} else {
+			Panel panel = widget.getPanel();
+			panel.addStyleName("swe-generic-vertical-panel");
+			contentPanel.add(panel);
+		}
+	}
+
+	private void handleTypeOf(ISensorWidget widget) {
+		if(widget.getType() == TAG_TYPE.ATTRIBUTE) {
+			if(widget.getName().equals("title")) {
+				linkName = widget.getValue("title", true);
+			} else if(widget.getName().equals("href")) {
+				href = widget.getValue("href", true);
+			}
+		}
+		if(getElements().isEmpty()) {
+			if(anchor == null) {
+				anchor = new Anchor("","");
+				HorizontalPanel hPanel = new HorizontalPanel();
+				
+				hPanel.add(anchor);
+				
+				VerticalPanel vPanel = new VerticalPanel();
+				vPanel.add(hPanel);
+				vPanel.addStyleName("swe-generic-vertical-panel");
+				contentPanel.add(vPanel);
+				
+				Panel advancedPanel = getEditPanel(new IButtonCallback() {
+					@Override
+					public void onClick() {
+						refreshChildren(getElements());
+						refreshParents(getParent());
+						
+						
+						for(ISensorWidget widget : getElements()) {
+							if(getName().equals("typeOf")) {
+								handleTypeOf(widget);
+							}
+						}
+					}
+				});
+				hPanel.add(advancedPanel);
+			} else {
+				anchor.setText("");
+				anchor.setHref("");
+			}
+			
+		} else {
+			//generate client link url
+			anchor.setHref(Utils.getCurrentURL(href));
+			anchor.setText(linkName);
+		}
+		
+		anchor.setTarget("_blank");
+	}
+	
+	private void handleAttribute(ISensorWidget widget) {
+		//get header from child value
+		if(!widget.getElements().isEmpty()) {
+			//the first one should be a value widget
+			String value = widget.getElements().get(0).getName();
+			//set header
+			hidePanel.getHeaderTextAccessor().setText(value);
+		}
+	}
+	
 	@Override
 	protected void activeMode(MODE mode) {
 		// TODO Auto-generated method stub
