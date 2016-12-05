7eb6379f84b34e18e199bfc7d30524033ef8432e
Mathieu Dhainaut <mathieu.dhainaut@gmail.com>
2016-12-05 15:41:15 +0100

    Add SWE:label panel

---
+++ src/com/sensia/tools/client/swetools/editors/sensorml/panels/swe/SWELabelPanel.java
@@ -0,0 +1,12 @@
+package com.sensia.tools.client.swetools.editors.sensorml.panels.swe;
+
+import com.sensia.relaxNG.RNGElement;
+import com.sensia.tools.client.swetools.editors.sensorml.panels.base.element.ElementPanel;
+
+public class SWELabelPanel extends ElementPanel{
+
+	public SWELabelPanel(RNGElement element) {
+		super(element);
+	}
+
+}
