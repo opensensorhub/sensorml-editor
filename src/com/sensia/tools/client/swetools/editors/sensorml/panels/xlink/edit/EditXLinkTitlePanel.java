91bdf3d602563ef905740b1a2cfe7e706b20f1a5
Mathieu Dhainaut <mathieu.dhainaut@gmail.com>
2016-12-08 13:09:16 +0100

    Add xlink:title edit panel

---
+++ src/com/sensia/tools/client/swetools/editors/sensorml/panels/xlink/edit/EditXLinkTitlePanel.java
@@ -0,0 +1,13 @@
+package com.sensia.tools.client.swetools.editors.sensorml.panels.xlink.edit;
+
+import com.sensia.relaxNG.RNGAttribute;
+import com.sensia.tools.client.swetools.editors.sensorml.panels.base.attribute.edit.EditAttributePanel;
+
+//TODO
+public class EditXLinkTitlePanel extends EditAttributePanel{
+
+	public EditXLinkTitlePanel(RNGAttribute tag) {
+		super(tag);
+	}
+
+}
