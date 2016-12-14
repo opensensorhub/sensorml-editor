d3fc905d354ae28689b30806790ec642acf9bcfd
Mathieu Dhainaut <mathieu.dhainaut@gmail.com>
2016-12-14 16:33:37 +0100

    Add styling for sml:component (edit mode)

--- src/com/sensia/tools/client/swetools/editors/sensorml/renderer/editor/panels/sml/SMLEditComponentPanel.java
+++ src/com/sensia/tools/client/swetools/editors/sensorml/renderer/editor/panels/sml/SMLEditComponentPanel.java
@@ -1,15 +1,8 @@
 package com.sensia.tools.client.swetools.editors.sensorml.renderer.editor.panels.sml;
 
-import com.google.gwt.user.client.ui.Anchor;
-import com.google.gwt.user.client.ui.Widget;
-import com.sensia.relaxNG.RNGAttribute;
 import com.sensia.relaxNG.RNGElement;
-import com.sensia.relaxNG.RNGTag;
 import com.sensia.tools.client.swetools.editors.sensorml.panels.AbstractPanel;
-import com.sensia.tools.client.swetools.editors.sensorml.panels.IPanel;
-import com.sensia.tools.client.swetools.editors.sensorml.panels.base.element.DisclosureElementPanel;
 import com.sensia.tools.client.swetools.editors.sensorml.renderer.editor.panels.element.EditSubSectionElementPanel;
-import com.sensia.tools.client.swetools.editors.sensorml.utils.Utils;
 
 /**
  * 
@@ -25,19 +18,9 @@ public class SMLEditComponentPanel extends EditSubSectionElementPanel{
 
 	public SMLEditComponentPanel(RNGElement tag) {
 		super(tag);
+		container.addStyleName("component");
 	}
 	
-	
-	@Override
-	public String getName() {
-		return getTag().getName();
-	}
-
-	@Override
-	protected void addInnerElement(IPanel<? extends RNGTag> element) {
-		super.addInnerElement(element);
-	}
-
 	@Override
 	protected AbstractPanel<RNGElement> newInstance() {
 		// TODO Auto-generated method stub
