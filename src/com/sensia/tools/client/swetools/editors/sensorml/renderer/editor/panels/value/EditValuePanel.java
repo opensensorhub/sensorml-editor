3ac64f822eeaa02d1f509463144eb4a001e115cc
Mathieu Dhainaut <mathieu.dhainaut@gmail.com>
2017-01-19 15:40:01 +0100

    [CSS] Align header remove button

--- src/com/sensia/tools/client/swetools/editors/sensorml/renderer/editor/panels/value/EditValuePanel.java
+++ src/com/sensia/tools/client/swetools/editors/sensorml/renderer/editor/panels/value/EditValuePanel.java
@@ -7,6 +7,7 @@ import com.google.gwt.event.dom.client.FocusHandler;
 import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.TextBox;
 import com.sensia.relaxNG.RNGData;
 import com.sensia.relaxNG.RNGTag;
