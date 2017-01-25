dda01de10b7c071f63a68058405d651f25cf29be
mdhsl <nevro@localhost.localdomain>
2017-01-25 16:07:16 +0100

    Add advanced datarecord/field renderer

---
+++ src/com/sensia/tools/client/swetools/editors/sensorml/renderer/simpledatatype/SimpleDataTypeRenderer.java
@@ -0,0 +1,164 @@
+/***************************** BEGIN LICENSE BLOCK ***************************
+
+ The contents of this file are Copyright (C) 2011 Sensia Software LLC.
+ All Rights Reserved.
+ 
+ Contributor(s): 
+    Alexandre Robin <alex.robin@sensiasoftware.com>
+ 
+ ******************************* END LICENSE BLOCK ***************************/
+
+package com.sensia.tools.client.swetools.editors.sensorml.renderer.simpledatatype;
+
+import java.util.List;
+
+import com.sensia.relaxNG.RNGAttribute;
+import com.sensia.relaxNG.RNGChoice;
+import com.sensia.relaxNG.RNGData;
+import com.sensia.relaxNG.RNGElement;
+import com.sensia.relaxNG.RNGOptional;
+import com.sensia.relaxNG.RNGTag;
+import com.sensia.relaxNG.RNGValue;
+import com.sensia.relaxNG.RNGZeroOrMore;
+import com.sensia.relaxNG.XSDAnyURI;
+import com.sensia.relaxNG.XSDBoolean;
+import com.sensia.relaxNG.XSDDateTime;
+import com.sensia.relaxNG.XSDDecimal;
+import com.sensia.relaxNG.XSDDouble;
+import com.sensia.relaxNG.XSDInteger;
+import com.sensia.relaxNG.XSDString;
+import com.sensia.tools.client.swetools.editors.sensorml.panels.generic.GenericVerticalContainerPanel;
+import com.sensia.tools.client.swetools.editors.sensorml.renderer.advanced.AdvancedRendererRNG;
+import com.sensia.tools.client.swetools.editors.sensorml.renderer.advanced.AdvancedRendererSML;
+import com.sensia.tools.client.swetools.editors.sensorml.renderer.advanced.panels.rng.RNGChoicePanel;
+import com.sensia.tools.client.swetools.editors.sensorml.renderer.advanced.panels.rng.RNGOptionalPanel;
+import com.sensia.tools.client.swetools.editors.sensorml.renderer.advanced.panels.rng.RNGZeroOrMorePanel;
+
+/**
+ * <p>
+ * <b>Title:</b> RNGRenderer
+ * </p>
+ * 
+ * <p>
+ * <b>Description:</b><br/>
+ * Renders content of an RNG grammar using GWT widgets
+ * </p>
+ * 
+ * <p>
+ * Copyright (c) 2011
+ * </p>.
+ *
+ * @author Alexandre Robin
+ * @date Aug 27, 2011
+ */
+public class SimpleDataTypeRenderer extends AdvancedRendererSML {
+	
+	public SimpleDataTypeRenderer() {
+		super();
+	}
+	/* (non-Javadoc)
+	 * @see com.sensia.relaxNG.RNGTagVisitor#visit(com.sensia.relaxNG.RNGElement)
+	 */
+	@Override
+	public void visit(RNGElement elt) {
+		visitChildren(elt.getChildren());
+	}
+	
+	/* (non-Javadoc)
+	 * @see com.sensia.relaxNG.RNGTagVisitor#visit(com.sensia.relaxNG.RNGAttribute)
+	 */
+	@Override
+	public void visit(RNGAttribute attribute) {
+		visitChildren(attribute.getChildren());
+	}
+	
+	/* (non-Javadoc)
+	 * @see com.sensia.relaxNG.RNGTagVisitor#visit(com.sensia.relaxNG.RNGValue)
+	 */
+	@Override
+	public void visit(RNGValue val) {
+	}
+
+	/* (non-Javadoc)
+	 * @see com.sensia.relaxNG.RNGTagVisitor#visit(com.sensia.relaxNG.RNGData)
+	 */
+	@Override
+	public void visit(RNGData<?> data) {
+	}
+
+	/* (non-Javadoc)
+	 * @see com.sensia.relaxNG.RNGTagVisitor#visit(com.sensia.relaxNG.XSDString)
+	 */
+	@Override
+	public void visit(XSDString data) {
+	}
+
+	/* (non-Javadoc)
+	 * @see com.sensia.relaxNG.RNGTagVisitor#visit(com.sensia.relaxNG.XSDBoolean)
+	 */
+	@Override
+	public void visit(XSDBoolean data) {
+	}
+
+	/* (non-Javadoc)
+	 * @see com.sensia.relaxNG.RNGTagVisitor#visit(com.sensia.relaxNG.XSDDecimal)
+	 */
+	@Override
+	public void visit(XSDDecimal data) {
+	}
+
+	/* (non-Javadoc)
+	 * @see com.sensia.relaxNG.RNGTagVisitor#visit(com.sensia.relaxNG.XSDDouble)
+	 */
+	@Override
+	public void visit(XSDDouble data) {
+	}
+
+	/* (non-Javadoc)
+	 * @see com.sensia.relaxNG.RNGTagVisitor#visit(com.sensia.relaxNG.XSDInteger)
+	 */
+	@Override
+	public void visit(XSDInteger data) {
+	}
+
+	/* (non-Javadoc)
+	 * @see com.sensia.relaxNG.RNGTagVisitor#visit(com.sensia.relaxNG.XSDAnyURI)
+	 */
+	@Override
+	public void visit(XSDAnyURI data) {
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see com.sensia.relaxNG.RNGTagVisitor#visit(com.sensia.relaxNG.XSDDateTime)
+	 */
+	@Override
+	public void visit(XSDDateTime data) {
+		
+	}
+	
+	/* (non-Javadoc)
+	 * @see com.sensia.relaxNG.RNGTagVisitor#visit(com.sensia.relaxNG.RNGChoice)
+	 */
+	@Override
+	public void visit(RNGChoice choice) {
+	    choice.combineNestedChoices();
+		push(new RNGChoicePanel(choice,getRefreshHandler()));
+	}
+
+	/* (non-Javadoc)
+	 * @see com.sensia.relaxNG.RNGTagVisitor#visit(com.sensia.relaxNG.RNGOptional)
+	 */
+	@Override
+	public void visit(RNGOptional optional) {
+		push(new RNGOptionalPanel(optional,getRefreshHandler()));
+	}
+
+	/* (non-Javadoc)
+	 * @see com.sensia.relaxNG.RNGTagVisitor#visit(com.sensia.relaxNG.RNGZeroOrMore)
+	 */
+	@Override
+	public void visit(RNGZeroOrMore zeroOrMore) {
+		push(new RNGZeroOrMorePanel(zeroOrMore,getRefreshHandler()));
+	}
+}
