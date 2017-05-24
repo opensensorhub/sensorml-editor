20e7b93d5eef52ce52d7aa2288d7f3e479b94d94
alexrobin <alex.robin@sensiasoft.com>
2017-05-24 13:45:45 +0200

    Show loading indicator while searching ontology

--- src/com/sensia/tools/client/swetools/editors/sensorml/ontology/OntologyPanel.java
+++ src/com/sensia/tools/client/swetools/editors/sensorml/ontology/OntologyPanel.java
@@ -1,10 +1,12 @@
 package com.sensia.tools.client.swetools.editors.sensorml.ontology;
 
 import java.util.List;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ChangeEvent;
 import com.google.gwt.event.dom.client.ChangeHandler;
 import com.google.gwt.event.logical.shared.ResizeEvent;
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.TextBox;
@@ -26,31 +28,32 @@ public class OntologyPanel {
 		ontologyTable = new GenericTable();
         ontologyTable.setEditable(false);
 		
-		// dialog panels
-		SMLHorizontalPanel hPanel;
-		
-        // source list
-		hPanel = new SMLHorizontalPanel();
-		ontologyPanel.add(hPanel);
-        hPanel.add(new HTML("Source:"+SensorConstants.HTML_SPACE));
+		// source list
+		SMLHorizontalPanel sourcesPanel = new SMLHorizontalPanel();
+		ontologyPanel.add(sourcesPanel);
+		sourcesPanel.add(new HTML("Source:"+SensorConstants.HTML_SPACE));
 		final ListBox sourcesBox = new ListBox();
         sourcesBox.addItem("XDOMES", "https://xdomes.org/sparql");
         sourcesBox.addItem("MMI", "http://mmisw.org/sparql");
         sourcesBox.setStyleName("ontology-sourcesBox");
-		hPanel.add(sourcesBox);
+        sourcesPanel.add(sourcesBox);
 		
 		// search box
-		hPanel = new SMLHorizontalPanel();
-		ontologyPanel.add(hPanel);
+		final SMLHorizontalPanel searchPanel = new SMLHorizontalPanel();
+		ontologyPanel.add(searchPanel);
 		final TextBox searchBox = new TextBox();
-        hPanel.add(new HTML("Search:"+SensorConstants.HTML_SPACE));
-        hPanel.add(searchBox);
+		searchPanel.add(new HTML("Search:"+SensorConstants.HTML_SPACE));
+		searchPanel.add(searchBox);
         searchBox.addChangeHandler(new ChangeHandler() {            
             @Override
             public void onChange(ChangeEvent event) {
                 String source = sourcesBox.getSelectedValue();
                 String searchTerm = searchBox.getValue();
                 
+                // show loading indicator
+                searchPanel.add(new Image(GWT.getModuleBaseURL()+"images/ajax-loader-small.gif"));
+                
+                // execute search query
                 IOntologySearch onto = new MMIRegistry(source);
                 onto.search(searchTerm, new ILoadOntologyCallback() {                    
                     @Override
@@ -59,7 +62,8 @@ public class OntologyPanel {
                         ontologyTable.setEditable(false);
                         Panel panelTable = ontologyTable.createTable();
                         ontologyTable.populateTable(headers, data);
-                        ontologyPanel.remove(2);
+                        ontologyPanel.remove(2); // remove old table
+                        searchPanel.remove(2); // remove loading indicator
                         ontologyPanel.add(panelTable);
                         ontologyPanel.fireEvent(new ResizeEvent(0,0) {});
                     }
@@ -79,6 +83,4 @@ public class OntologyPanel {
 	public Panel getPanel() {
 		return ontologyPanel;
 	}
-	
-	//ALGO PART
 }
